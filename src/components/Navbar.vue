ad30f12a65e688fc6b6ce85f2428fbed82ea1f38
Salih Gumus <sudanmerinosu@gmail.com>
2022-05-15 20:39:54 +0300

    [feature][Salih] wip

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,42 +1,52 @@
 <template>
-
-	<nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded ">
+	<nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded">
 		<div class="container flex flex-wrap justify-between items-center mx-auto">
 			<a href="" class="flex items-center">
-				<img src="../assets/karpuz.png" class="mr-3 h-6 sm:h-9"
-					alt="Karpuz Logo">
+				<img
+					src="../assets/karpuz.png"
+					class="mr-3 h-6 sm:h-9"
+					alt="Karpuz Logo"
+				/>
 				<span
-					class="self-center text-xl font-semibold whitespace-nowrap ">Karpuz</span>
+					class="self-center text-xl font-semibold whitespace-nowrap text-gray-600"
+					>Karpuz</span
+				>
 			</a>
 			<div class="flex items-center md:order-2" v-if="false">
-				<img class="w-8 h-8 rounded-full"
+				<img
+					class="w-8 h-8 rounded-full"
 					src="https://avatars.dicebear.com/api/big-smile/male/sudanmerinosu.svg?b=%23c8ccd5&r=50&scale=82"
-					alt="user photo">
+					alt="user photo"
+				/>
 			</div>
 			<div>
-				<button type="button"
-					class=" text-white bg-red-500 hover:bg-red-400 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-2.5 text-center ">Login</button>
+				<button
+					type="button"
+					class="text-white bg-red-500 hover:bg-red-400 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-2.5 text-center"
+				>
+					Login
+				</button>
 
-				<button type="button"
-					class="text-red-500 hover:text-white border border-red-400 hover:bg-red-400 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-2 text-center ml-2 ">Register</button>
+				<button
+					type="button"
+					class="text-red-500 hover:text-white border border-red-400 hover:bg-red-400 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-2 text-center ml-2"
+				>
+					Register
+				</button>
 			</div>
 		</div>
 	</nav>
-
-
 </template>
 
 <script setup>
-import { useTemplateStore } from '../stores/template';
+	import { useTemplateStore } from '../stores/template';
 
-const store = useTemplateStore();
-// console.log(store);
+	const store = useTemplateStore();
+	// console.log(store);
 
-defineProps({
-	msg: String,
-});
+	defineProps({
+		msg: String,
+	});
 </script>
 
-
-<style scoped>
-</style>
+<style scoped></style>
