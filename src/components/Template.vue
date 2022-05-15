74beb9053d99d1a08b7510eb85ab82144393dc0f
Salih Gumus <sudanmerinosu@gmail.com>
2022-05-15 17:18:12 +0300

    [feature][Salih] add tailwind via cdn

--- src/components/Template.vue
+++ src/components/Template.vue
@@ -10,11 +10,12 @@ defineProps({
 </script>
 
 <template>
-	<h1 class="ext-3xl font-bold underline">{{ msg }}</h1>
+	<h1 class="text-3xl font-bold underline">{{ msg }}</h1>
 
 	<h2>Count is {{ store.count }}</h2>
 	<h2>Double count is {{ store.doubleCount }}</h2>
 	<button @click="store.increment(1)">Increment</button>
 </template>
 
-<style scoped></style>
+<style scoped>
+</style>
