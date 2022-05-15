91dad1dc52fbc6ba0a5438e470a4ee0f70309ffa
Salih Gumus <sudanmerinosu@gmail.com>
2022-05-15 14:54:39 +0300

    [feature][Salih] add tailwind

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+
 import App from './App.vue';
 
 import router from './router';
+
+import './css/main.css';
 
 const app = createApp(App);
 app.use(router);
