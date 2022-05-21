66b535d1679d545c80cdcf43c58d20220578ad09
Salih Gumus <sudanmerinosu@gmail.com>
2022-05-21 16:05:19 +0300

    [feature][Salih] add register page

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,11 @@ const routes = [
 		name: 'login',
 		component: () => import('../views/Login.vue'),
 	},
+	{
+		path: '/register',
+		name: 'register',
+		component: () => import('../views/Register.vue'),
+	},
 ];
 
 const router = createRouter({
