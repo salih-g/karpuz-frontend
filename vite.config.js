b15b7f89680566c2107e71119c45e42a8c81d3d2
Salih Gumus <sudanmerinosu@gmail.com>
2022-05-28 18:32:40 +0300

    [feature][Salih] add @prefix

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from 'url';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
@@ -13,4 +14,9 @@ export default defineConfig({
 			},
 		}),
 	],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+		},
+	},
 });
