46bb4438597be197231d4d866fe0f2607a55d8b6
M.Salih Gumus <gumusmuhammedsalih@gmail.com>
2022-07-04 21:58:25 +0300

    [feature][salih] remove not found

---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+	<h1 class="text-center">Not Found</h1>
+</template>
+
+<script setup>
+	import { onMounted } from 'vue';
+	import { useRouter } from 'vue-router';
+	const router = useRouter();
+
+	onMounted(() => {
+		router.push({ path: '/' });
+	});
+</script>
+
+<style></style>
