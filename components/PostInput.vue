134756261eaec68f0509436bac5bd00044e3a770
M.Salih Gumus <gumusmuhammedsalih@gmail.com>
2022-08-23 22:13:58 +0300

    [feature][Salih] update post input ✨

--- components/PostInput.vue
+++ components/PostInput.vue
@@ -1,16 +1,22 @@
 <template>
 	<form
-		class="bg-white shadow rounded-lg mb-6 p-4 w-full md:w-3/4 mt-10 text-gray-600"
+		class="card bg-white rounded-lg mb-6 p-4 w-full md:w-3/4 mt-10 text-gray-600"
 	>
 		<textarea
 			name="message"
 			placeholder="Type something..."
-			class="focus:outline-none w-full rounded-lg p-2 text-sm bg-gray-100 border border-transparent appearance-none rounded-tg placeholder-gray-400"
+			class="focus:outline-none w-full rounded-lg p-2 text-sm bg-gray-100 border border-transparent appearance-none rounded-tg placeholder-gray-400 textarea"
 			maxlength="240"
 			rows="3"
 		></textarea>
 		<!-- footer -->
-		<div class="flex justify-end mt-2">
+		<div class="flex justify-between mt-2">
+			<div class="flex gap-2 mt-1">
+				<processbar
+					class="radial-progress text-error"
+					style="--value: 10; --size: 2rem; --thickness: 6px"
+				></processbar>
+			</div>
 			<button
 				class="flex items-center py-2 px-4 rounded-lg text-sm bg-red-500 text-white shadow-lg"
 			>
