091b7b0101b4767dfbeb83117bb244b69923b2ab
M.Salih Gumus <gumusmuhammedsalih@gmail.com>
2022-08-23 20:22:51 +0300

    [feature][Salih] add daisyui ✨

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,19 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-})
+	head: {
+		title: 'Karpuz',
+		htmlAttrs: {
+			lang: 'en',
+			'data-theme': 'karpuzTheme',
+		},
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ hid: 'description', name: 'description', content: '' },
+			{ name: 'format-detection', content: 'telephone=no' },
+		],
+		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+	},
+});
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+	daisyui: {
+		themes: [
+			{
+				karpuzTheme: {
+					primary: '#f66667',
+					secondary: '#89edc8',
+					accent: '#bbf57b',
+					neutral: '#261E29',
+					'base-100': '#202024',
+					info: '#4b5564',
+					success: '#408001',
+					warning: '#F1C946',
+					error: '#f67171',
+				},
+			},
+		],
+	},
+	plugins: [require('daisyui')],
+};
